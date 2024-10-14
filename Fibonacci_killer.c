11a9461a86ee58a22d7751f9d02d21b0d6ce072b
CenZhichu <cenzhichu@outloook.com>
2024-10-14 17:28:23 +0800

    oct14

---
+++ Fibonacci_killer.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+unsigned long Fibonacci(unsigned n);
+
+int main(void)
+{
+    printf("%lu\n",Fibonacci(80));
+    return 0;
+}
+
+unsigned long Fibonacci(unsigned n)
+{
+    if (n > 2)
+    {
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+    }
+    else
+    {
+        return 1;
+    }
+}
