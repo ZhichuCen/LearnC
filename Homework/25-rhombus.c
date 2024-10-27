4533631aa3f17561055a38641f549eeb28f89655
CenZhichu <cenzhichu@outloook.com>
2024-10-27 18:58:35 +0800

    add comment

--- Homework/25-rhombus.c
+++ Homework/25-rhombus.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 
-//这是声明，防止编译器不知道函数怎么用
+//这是函数声明，防止编译器不知道函数怎么用，新版编译器不需要声明。
 int print_rhomos(int n, int h);
 int print_line(int n,int h);
 
-int print_rhomos(int n, int h){
+int print_rhomos(int n, int h){//这是主体函数
 
-    if(h==n){
+    if(h==n){//这是递归的结束条件
         print_line(n,h);
         return 0;
     }
 
     print_line(n,h);
     print_rhomos(n,h+1);//这里是递归
-    print_line(n,h);
+    print_line(n,h);//嘻嘻这才是精髓
     return 0;
 }
 
-int print_line(int n,int h){
+int print_line(int n,int h){//这是打印单行的代码
     for(int i=0;i<n-h;i++){
         putchar(' ');
         putchar(' ');
@@ -39,7 +39,6 @@ int print_line(int n,int h){
 int main(void){
     int n;
     scanf("%d",&n);
-    // n=3;
 
     print_rhomos(n,1);
 
